1c19427dec5cb87e79c6395c3d8b540d497694a4
MahoMori <tr27e1104@gmail.com>
2021-10-26 12:39:46 -0700

    added model (except img)

--- myfav/models.py
+++ myfav/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Myfav(models.Model):
+
+    genre_choices = (
+        ('book', 'Book'),
+        ('anime/manga', 'Anime/Manga'),
+        ('movie', 'Movie'),
+        ('music', 'Music'),
+        ('game', 'Game'),
+        ('food', 'Food'),
+        ('other', 'Other')
+    )
+
+    name= models.CharField(max_length=100)
+    author_or_creator= models.CharField(max_length=100, blank=True)
+    genre= models.CharField(max_length=15, choices=genre_choices, default='book')
+    # image= models.ImageField(upload_to='images')
+    description= models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
--- myfav/views.py
+++ myfav/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def myfavlist(request):
+    return render(request, 'myfav/myfav.html')
