ded688f0bcbfc7411cd79ce32f4a9b9d13c406b4
MahoMori <tr27e1104@gmail.com>
2021-10-28 10:24:12 -0700

    modifing edit page

--- myfavlist/settings.py
+++ myfavlist/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myfav'
+    'myfav',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
